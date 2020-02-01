d9ba8aea892ffe194a88231de08b8bab9eb26f2f
saj <sajsxj@nyist.edu.cn>
2020-02-01 14:32:44 +0800

    first commit to git

---
+++ src/com/saj/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.saj.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
